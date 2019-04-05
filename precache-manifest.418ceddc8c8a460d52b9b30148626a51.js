a90fb30a3268e918c7dbe84aacbf70c79dfc180b
Edwin Agbenyega <eddybanks03@gmail.com>
2019-04-04 22:11:19 -0600

    Updates

---
+++ precache-manifest.418ceddc8c8a460d52b9b30148626a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7106d41ae14b17da155",
+    "url": "eddybanks.github.io/kirtland-logistics/static/js/runtime~main.41763aeb.js"
+  },
+  {
+    "revision": "3f2f9f73e60d179eb50d",
+    "url": "eddybanks.github.io/kirtland-logistics/static/js/main.4929b6d0.chunk.js"
+  },
+  {
+    "revision": "04df906ce648de8a55e7",
+    "url": "eddybanks.github.io/kirtland-logistics/static/js/2.be5aef1e.chunk.js"
+  },
+  {
+    "revision": "3f2f9f73e60d179eb50d",
+    "url": "eddybanks.github.io/kirtland-logistics/static/css/main.3ba3dc2e.chunk.css"
+  },
+  {
+    "revision": "04df906ce648de8a55e7",
+    "url": "eddybanks.github.io/kirtland-logistics/static/css/2.b7ad565d.chunk.css"
+  },
+  {
+    "revision": "a41be545b8e42fd602c5f7f31dd6addd",
+    "url": "eddybanks.github.io/kirtland-logistics/index.html"
+  }
+];
