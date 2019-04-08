7196cb13b26939ccf81681b919585391230381b9
Edwin Agbenyega <eddybanks03@gmail.com>
2019-04-08 11:18:56 -0600

    Figure out tabs in Zone components

--- src/components/home/Zone/Zone.js
+++ src/components/home/Zone/Zone.js
@@ -3,23 +3,28 @@ import styles from '../../../stylesheets/Zone.module.css'
 import Header from '../../layouts/Header'
 import ZoneTable from './ZoneTable'
 import ZoneTableEdit from './ZoneTableEdit'
-import { Container, Row, Col, Table, Button, ButtonGroup } from 'reactstrap'
+import { Container, Row, Col, Button, ButtonGroup, TabPane, TabContent, NavLink, Nav } from 'reactstrap'
+import classnames from 'classnames'
 
 const zone = props => {
   let editTitle = props.editMode ? "Edit Mode" : "View Mode"
-  let zoneTable = props.zones.map(zone => (
-    <Col className="col-md-12"> 
-      { props.editMode ?
-      <ZoneTableEdit 
-        key={"zone_edit " + zone.id} 
-        lots={zone.lots} 
-        titles={props.lot_titles} /> :
-      <ZoneTable 
-        key={"zone " + zone.id} 
-        lots={zone.lots} 
-        titles={props.lot_titles} />
-      }
-    </Col>
+  let zoneTable = props.zones.map((zone, index) => (
+    <TabPane tabId={"tab " + index} key={"tabkey "+ zone.id}>
+      <Row className="mt-4">
+        <Col className="col-md-12"> 
+          { props.editMode ?
+          <ZoneTableEdit 
+            key={"zone_edit " + zone.id} 
+            lots={zone.lots} 
+            titles={props.lot_titles} /> :
+          <ZoneTable 
+            key={"zone " + zone.id} 
+            lots={zone.lots} 
+            titles={props.lot_titles} />
+          }
+        </Col>
+      </Row>
+    </TabPane>
   ))
   return (
     <Container>
@@ -28,16 +33,22 @@ const zone = props => {
         <Row>
           <Col className="md-12">
             <ButtonGroup>
-              { props.zones.map(zone => (
-                <Button key={"zone_name " + zone.id}>{zone.name}</Button>
+              { props.zones.map((zone, index) => (
+                <Button 
+                  key={"zone_name " + zone.id} 
+                  className={classnames({ active: props.activeZoneTab === "tab " + index })}
+                  onClick={() => { props.toggleZoneTab("tab " + index) }}
+                >
+                  {zone.name}
+                </Button>
               ))}
               <Button outline onClick={props.toggleEdit}>{editTitle}</Button>
             </ButtonGroup>
           </Col>
         </Row>
-        <Row className="mt-4">
+        <TabContent activeTab={props.activeZoneTab}>
           {zoneTable}
-        </Row>
+        </TabContent>
       </section>
     </Container>
   )
--- src/components/home/Zone/ZoneContainer.js
+++ src/components/home/Zone/ZoneContainer.js
@@ -6,8 +6,11 @@ class ZoneContainer extends Component {
     super(props)
 
     this.toggleEditHandler = this.toggleEditHandler.bind(this)
+    this.toggleZoneTab = this.toggleZoneTab.bind(this)
 
     this.state = {
+      // For toggling between selected Zones
+      activeZoneTab: 'tab 0',
       // For toggling between Edit and View Modes
       editMode: false,
       lot_titles: [
@@ -48,24 +51,24 @@ class ZoneContainer extends Component {
         lots: [
           { id: 1, name: 'A', 
             details: [
-              { id: 1, title: 'Vehicle Capacity', value: 20, type: 'number' },
-              { id: 2, title: 'Current Vehicle Count', value: 12, type: 'number'},
+              { id: 1, title: 'Vehicle Capacity', value: 21, type: 'number' },
+              { id: 2, title: 'Current Vehicle Count', value: 32, type: 'number'},
               { id: 3, title: 'Estimated Travel Distance', value: 23, type: 'number'},
               { id: 4, title: 'Estimated Travel Time', value: 20, type: 'number'}
             ]
           },
           { id: 2, name: 'B', 
             details: [
-              { id: 1, title: 'Vehicle Capacity', value: 20, type: 'number' },
-              { id: 2, title: 'Current Vehicle Count', value: 12, type: 'number'},
+              { id: 1, title: 'Vehicle Capacity', value: 43, type: 'number' },
+              { id: 2, title: 'Current Vehicle Count', value: 24, type: 'number'},
               { id: 3, title: 'Estimated Travel Distance', value: 23, type: 'number'},
-              { id: 4, title: 'Estimated Travel Time', value: 20, type: 'number'}
+              { id: 4, title: 'Estimated Travel Time', value: 12, type: 'number'}
             ]
           },
           { id: 3, name: 'C', 
             details: [
-              { id: 1, title: 'Vehicle Capacity', value: 20, type: 'number' },
-              { id: 2, title: 'Current Vehicle Count', value: 12, type: 'number'},
+              { id: 1, title: 'Vehicle Capacity', value: 43, type: 'number' },
+              { id: 2, title: 'Current Vehicle Count', value: 43, type: 'number'},
               { id: 3, title: 'Estimated Travel Distance', value: 23, type: 'number'},
               { id: 4, title: 'Estimated Travel Time', value: 20, type: 'number'}
             ]
@@ -73,6 +76,14 @@ class ZoneContainer extends Component {
         ]
       }
       ]      
+    }
+  }
+
+  toggleZoneTab = (tab) => {
+    if (this.state.activeZoneTab !== tab) {
+      this.setState({
+        activeZoneTab: tab
+      })
     }
   }
 
@@ -93,10 +104,12 @@ class ZoneContainer extends Component {
     return (
       <Fragment>
         <Zone zones={this.state.zones} 
+          activeZoneTab={this.state.activeZoneTab}
           lot_titles={this.state.lot_titles} 
           editMode={this.state.editMode}
           toggleEdit={this.toggleEditHandler}
-          inputLot={this.inputLotHandler} />
+          inputLot={this.inputLotHandler}
+          toggleZoneTab={this.toggleZoneTab} />
       </Fragment>
     )
   }
