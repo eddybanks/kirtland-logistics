5bac70bf578850ad5b2f7cb6bcd0509e993f921f
Edwin Agbenyega <eddybanks03@gmail.com>
2019-04-07 21:48:20 -0600

    Modify FormInput component to accept props

--- src/components/forms/FormInput.js
+++ src/components/forms/FormInput.js
@@ -3,8 +3,20 @@ import { Input } from 'reactstrap'
 
 const FormInput = props => {
   if (props.type == 'number') {
-    return <Input type='number' defaultValue={props.defaultValue} />
-  } else {
+    return (
+      <Input 
+        type='number' 
+        id={props.id}
+        min={props.min}
+        max={props.max}
+        defaultValue={props.defaultValue} />
+      )
+  } else if (props.type == 'string') {
+    return <Input />
+  } else if (props.type == 'time') {
+    return <Input />
+  }
+  else {
     return <Input />
   }
 }
